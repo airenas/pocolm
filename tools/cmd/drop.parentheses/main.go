1c95b2a5225e7ee49d47d1468def8113b3726811
airenas <airenass@gmail.com>
2020-05-15 17:48:46 +0300

    Refactor tools

--- tools/cmd/drop.parentheses/main.go
+++ tools/cmd/drop.parentheses/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"io"
-	"log"
-	"os"
 	"strings"
 
-	"github.com/airenas/pocolm/tools/internal/pkg/util"
-	"github.com/pkg/errors"
+	"github.com/airenas/pocolm/tools/internal/pkg/cmd"
 )
 
 var parenthesesSymbols map[rune]rune
@@ -24,44 +17,11 @@ func init() {
 }
 
 func main() {
-	log.SetOutput(os.Stderr)
-	fs := flag.CommandLine
-	fs.Usage = func() {
-		fmt.Fprintf(fs.Output(), "Usage of %s: [input-file | stdin] [output-file | stdout]\n", os.Args[0])
-		fs.PrintDefaults()
-	}
-	fs.Parse(os.Args[1:])
-	f, err := util.NewReadWrapper(flag.Arg(0))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
+	cmd.ProcessByLine(processLine)
+}
 
-	destination, err := util.NewWriteWrapper(flag.Arg(1))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer destination.Close()
-
-	rd := bufio.NewReader(f)
-	ln := 0
-	for {
-		ln++
-		line, err := rd.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatal(errors.Wrapf(err, "Line %d", ln))
-		}
-		line = strings.TrimSpace(line)
-		nLine := changeLine(line)
-		_, err = fmt.Fprintf(destination, "%s\n", nLine)
-		if err != nil {
-			log.Fatal(errors.Wrap(err, "Cant write file"))
-		}
-	}
-	log.Printf("Done")
+func processLine(line string) (string, error) {
+	return changeLine(strings.TrimSpace(line)), nil
 }
 
 func changeLine(line string) string {
---
+++ tools/internal/pkg/cmd/byLine.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/airenas/pocolm/tools/internal/pkg/util"
+	"github.com/pkg/errors"
+)
+
+//ProcessByLine main function for tool to read file and write file
+func ProcessByLine(procF func(string) (string, error)) {
+	log.SetOutput(os.Stderr)
+	fs := flag.CommandLine
+	fs.Usage = func() {
+		fmt.Fprintf(fs.Output(), "Usage of %s: [input-file | stdin] [output-file | stdout]\n", os.Args[0])
+		fs.PrintDefaults()
+	}
+	fs.Parse(os.Args[1:])
+	f, err := util.NewReadWrapper(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	destination, err := util.NewWriteWrapper(flag.Arg(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer destination.Close()
+
+	rd := bufio.NewReader(f)
+	ln := 0
+	for {
+		ln++
+		line, err := rd.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(errors.Wrapf(err, "Line %d", ln))
+		}
+		nLine, err := procF(line)
+		if err != nil {
+			log.Fatal(errors.Wrapf(err, "Line %d", ln))
+		}
+		_, err = fmt.Fprintf(destination, "%s\n", nLine)
+		if err != nil {
+			log.Fatal(errors.Wrap(err, "Cant write file"))
+		}
+	}
+	log.Printf("Done")
+}
