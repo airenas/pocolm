2a9db590977cf4da832523357b6992160ca5b2ed
airenas <airenass@gmail.com>
2020-05-19 12:32:21 +0300

    Add params to app

--- tools/cmd/map.symbols/main.go
+++ tools/cmd/map.symbols/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"regexp"
 	"strings"
@@ -18,29 +19,44 @@ type replace struct {
 var replaces []*replace
 var regDots *regexp.Regexp
 
+type params struct {
+	changeEmail      bool
+	changeURL        bool
+	changeNumeration bool
+}
+
 func main() {
+	prms := params{}
+	flag.BoolVar(&prms.changeEmail, "email", false, "change emails to <EMAIL>")
+	flag.BoolVar(&prms.changeURL, "url", false, "change URLs to <URL>")
+	flag.BoolVar(&prms.changeNumeration, "num", false, "change initial numeration to <NUMERACIJA>")
 	cmd.InitApp()
-	initRegexp()
+	initRegexp(&prms)
 	cmd.ProcessByLine(processLine)
 }
 
-func initRegexp() {
+func initRegexp(p *params) {
 	romanNumbers := "M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})"
 	replaces = make([]*replace, 0)
-	replaces = append(replaces, &replace{str: "<EMAIL>", regxp: util.EMailRegexp})
-	replaces = append(replaces, &replace{str: "<EMAIL>", regxp: newRegexp("mailto:<EMAIL>")})
-	replaces = append(replaces, &replace{str: "<URL>", regxp: xurls.Strict()})
+	if p.changeEmail {
+		replaces = append(replaces, &replace{str: "<EMAIL>", regxp: util.EMailRegexp})
+		replaces = append(replaces, &replace{str: "<EMAIL>", regxp: newRegexp("mailto:<EMAIL>")})
+	}
+	if p.changeURL {
+		replaces = append(replaces, &replace{str: "<URL>", regxp: xurls.Strict()})
+	}
+	if p.changeNumeration {
+		replaces = append(replaces, &replace{str: " <NUMERACIJA> ", regxp: newRegexp("(?i)^[A-Z][\\)\\.]")})
+		replaces = append(replaces, &replace{str: " <NUMERACIJA> ", regxp: newRegexp("(?i)^" + romanNumbers + "[\\.)]")})
+
+		replaces = append(replaces, &replace{str: " <NUMERACIJA> ", regxp: newRegexp("^(([0-9]){1,3}\\.){1,}")})
+		replaces = append(replaces, &replace{str: " <NUMERACIJA> ", regxp: newRegexp("^([0-9]){1,3}\\)")})
+		replaces = append(replaces, &replace{str: " <NUMERACIJA> $3", regxp: newRegexp("^(([0-9]){1,3})( priedas.)")})
+		replaces = append(replaces, &replace{str: " <NUMERACIJA> $5", regxp: newRegexp("^(" + romanNumbers + ")( skyrius.)")})
+		replaces = append(replaces, &replace{str: " <PUNKTAS> ", regxp: newRegexp("^[\\*-] ")})
+	}
+
 	replaces = append(replaces, &replace{str: " <PILDOMA> ", regxp: newRegexp("[_]{2,}|[\\.]{4,}| [\\.]{3,}")})
-
-	replaces = append(replaces, &replace{str: " <NUMERACIJA> ", regxp: newRegexp("(?i)^[A-Z][\\)\\.]")})
-	replaces = append(replaces, &replace{str: " <NUMERACIJA> ", regxp: newRegexp("(?i)^" + romanNumbers + "[\\.)]")})
-
-	replaces = append(replaces, &replace{str: " <NUMERACIJA> ", regxp: newRegexp("^(([0-9]){1,3}\\.){1,}")})
-	replaces = append(replaces, &replace{str: " <NUMERACIJA> ", regxp: newRegexp("^([0-9]){1,3}\\)")})
-	replaces = append(replaces, &replace{str: " <NUMERACIJA> $3", regxp: newRegexp("^(([0-9]){1,3})( priedas.)")})
-	replaces = append(replaces, &replace{str: " <NUMERACIJA> $5", regxp: newRegexp("^(" + romanNumbers + ")( skyrius.)")})
-	replaces = append(replaces, &replace{str: " <PUNKTAS> ", regxp: newRegexp("^[\\*-] ")})
-
 	replaces = append(replaces, &replace{str: " ",
 		regxp: newRegexp("HYPERLINK|FORMTEXT|PAGEREF _Toc\\d+ \\\\h \\d+|MERGEFIELD [„]?[\\p{L}_\\d]+[“]?")})
 
--- tools/cmd/map.symbols/main_test.go
+++ tools/cmd/map.symbols/main_test.go
@@ -7,37 +7,37 @@ import (
 )
 
 func TestNoChange(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{})
 	assert.Equal(t, "mama", changeLine("mama"))
 }
 
 func TestURL(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeURL: true})
 	assert.Equal(t, "mama <URL>", changeLine("mama http://www.delfi.lt"))
 	assert.Equal(t, "mama <URL> ir", changeLine("mama http://delfi.lt?olia?tatata=tatat  ir"))
 	assert.Equal(t, "mama www.delfi.lt", changeLine("mama www.delfi.lt"))
 }
 
 func TestEmail(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeEmail: true})
 	assert.Equal(t, "mama <EMAIL>", changeLine("mama a@a.lt"))
 	assert.Equal(t, "mama <EMAIL> ir", changeLine("mama ai@delfi.lt  ir"))
 	assert.Equal(t, "<EMAIL> olia", changeLine("aaa@adelfi.lt olia"))
 }
 
 func TestEmailFix(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeEmail: true})
 	assert.Equal(t, "mama <EMAIL>", changeLine("mama mailto:a@a.lt"))
 }
 
 func TestUnderscore(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeNumeration: true})
 	assert.Equal(t, "mama <PILDOMA> <PILDOMA>", changeLine("mama __ _______"))
 	assert.Equal(t, "mama <PILDOMA>", changeLine("mama________"))
 }
 
 func TestDot(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{})
 	assert.Equal(t, "mama <PILDOMA>", changeLine("mama ...."))
 	assert.Equal(t, "mama..", changeLine("mama.."))
 	assert.Equal(t, "mama...", changeLine("mama..."))
@@ -48,7 +48,7 @@ func TestDot(t *testing.T) {
 }
 
 func TestNumberLetter(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeNumeration: true})
 	assert.Equal(t, "<NUMERACIJA> mama", changeLine("a) mama"))
 	assert.Equal(t, "aa) mama", changeLine("aa) mama"))
 	assert.Equal(t, "<NUMERACIJA> mama", changeLine("E) mama"))
@@ -57,7 +57,7 @@ func TestNumberLetter(t *testing.T) {
 }
 
 func TestNumber(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeNumeration: true})
 	assert.Equal(t, "<NUMERACIJA> mama", changeLine("1. mama"))
 	assert.Equal(t, "2005. mama", changeLine("2005. mama"))
 	assert.Equal(t, "<NUMERACIJA> mama", changeLine("1.2.3. mama"))
@@ -68,17 +68,17 @@ func TestNumber(t *testing.T) {
 }
 
 func TestStar(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeNumeration: true})
 	assert.Equal(t, "<PUNKTAS> mama", changeLine("* mama"))
 }
 
 func TestDash(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeNumeration: true})
 	assert.Equal(t, "<PUNKTAS> mama", changeLine("- mama"))
 }
 
 func TestNumberRoman(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeNumeration: true})
 	assert.Equal(t, "<NUMERACIJA> mama", changeLine("IX. mama"))
 	assert.Equal(t, "<NUMERACIJA> mama", changeLine("IX) mama"))
 	assert.Equal(t, "<NUMERACIJA> mama", changeLine("ix. mama"))
@@ -86,34 +86,34 @@ func TestNumberRoman(t *testing.T) {
 }
 
 func TestNumberRomanPart(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeNumeration: true})
 	assert.Equal(t, "<NUMERACIJA> skyrius. mama", changeLine("IX skyrius. mama"))
 }
 
 func TestAppendix(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{changeNumeration: true})
 	assert.Equal(t, "<NUMERACIJA> priedas. mama", changeLine("8 priedas. mama"))
 }
 
 func TestDropHyperlink(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{})
 	assert.Equal(t, "mama", changeLine("mama HYPERLINK"))
 }
 
 func TestDropFormtext(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{})
 	assert.Equal(t, "mama", changeLine("mama FORMTEXT"))
 }
 
 func TestDropPageRef(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{})
 	assert.Equal(t, "mama", changeLine("mama PAGEREF _Toc305158161 \\h 29"))
 	assert.Equal(t, "mama", changeLine("mama PAGEREF _Toc305158161 \\h 2"))
 	assert.Equal(t, "mama aaaa", changeLine("mama PAGEREF _Toc305158161 \\h 2 aaaa"))
 }
 
 func TestDropMergefield(t *testing.T) {
-	initRegexp()
+	initRegexp(&params{})
 	assert.Equal(t, "mama", changeLine("mama MERGEFIELD „Turto_aprašas“"))
 	assert.Equal(t, "mama", changeLine("mama MERGEFIELD Turto_aprašas"))
 	assert.Equal(t, "mama aaaa", changeLine("mama MERGEFIELD „Turto_aprašas“ aaaa"))
