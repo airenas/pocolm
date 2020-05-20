be2c774ce9e95bbeca1a6f7b3f0269c501ad9c0d
airenas <airenass@gmail.com>
2020-05-20 14:04:50 +0300

    Refactor punct tool

--- tools/internal/pkg/punct/punct.go
+++ tools/internal/pkg/punct/punct.go
@@ -3,6 +3,10 @@ package punct
 //Punctuations keep punctuation symbols
 var Punctuations map[rune]bool
 
+var sepSymbols map[rune]bool
+
+var parenthesesSymbols map[rune]bool
+
 const (
 	//StartQuote for LT
 	StartQuote = '„'
@@ -12,30 +16,57 @@ const (
 
 func init() {
 	Punctuations = make(map[rune]bool)
-	for _, r := range ",.!?-:;" {
+	for _, r := range ",./\\;:*&^%$#@!~?-+" {
 		Punctuations[r] = true
+	}
+	parenthesesSymbols = make(map[rune]bool)
+	for _, r := range []rune("[]{}<>()") {
+		parenthesesSymbols[r] = true
 	}
 }
 
-//PureWord retun word without punctuation
-func PureWord(w string) (string, string) {
-	rs := []rune(w)
-	l := len(rs)
-	for ; l > 0 && IsPunct(rs[l-1]); l-- {
+//PureWord retun word without punctuation, word, <string before word>, <string after word>
+func PureWord(w string) (string, string, string) {
+	return TrimWord(w, IsAllSep)
+}
+
+//TrimWord return word without runes matching f, word, <string before word>, <string after word>
+func TrimWord(w string, f func(rune) bool) (string, string, string) {
+	rns := []rune(w)
+	i1, i2 := 0, len(rns)-1
+	for ; i1 < len(rns) && f(rns[i1]); i1++ {
 	}
-	end := rs[l:]
-	rs = rs[0:l]
-	if len(rs) > 0 && rs[0] == StartQuote {
-		rs = rs[1:]
+	for ; i2 > i1 && f(rns[i2]); i2-- {
 	}
-	if len(rs) > 0 && rs[len(rs)-1] == EndQuote {
-		rs = rs[:len(rs)-1]
+	return string(rns[i1 : i2+1]), string(rns[:i1]), string(rns[i2+1:])
+}
+
+//TrimQuote return word without lt quotes
+func TrimQuote(w string) string {
+	rns := []rune(w)
+	i1, i2 := 0, len(rns)-1
+	if rns[i1] == StartQuote {
+		i1++
 	}
-	return string(rs), string(end)
+	if rns[i2] == EndQuote {
+		i2--
+	}
+	return string(rns[i1 : i2+1])
 }
 
 //IsPunct return true is rune is punctuation
 func IsPunct(r rune) bool {
 	_, ok := Punctuations[r]
 	return ok
+}
+
+//IsParentheses return true is rune is parentheses
+func IsParentheses(r rune) bool {
+	_, ok := parenthesesSymbols[r]
+	return ok
+}
+
+//IsAllSep return true is rune is parentheses or punctuation
+func IsAllSep(r rune) bool {
+	return IsParentheses(r) || IsPunct(r)
 }
--- tools/internal/pkg/punct/punct_test.go
+++ tools/internal/pkg/punct/punct_test.go
@@ -7,19 +7,45 @@ import (
 )
 
 func TestPureWord(t *testing.T) {
-	w, e := PureWord("mama.")
+	w, b, e := PureWord("mama.")
 	assert.Equal(t, "mama", w)
+	assert.Equal(t, "", b)
 	assert.Equal(t, ".", e)
-	w, e = PureWord("mama")
+	w, b, e = PureWord("mama")
 	assert.Equal(t, "mama", w)
+	assert.Equal(t, "", b)
 	assert.Equal(t, "", e)
-	w, e = PureWord("„mama“.,")
+}
+
+func TestPureWordStartEnd(t *testing.T) {
+	w, b, e := PureWord("(,mama.")
 	assert.Equal(t, "mama", w)
-	assert.Equal(t, ".,", e)
+	assert.Equal(t, "(,", b)
+	assert.Equal(t, ".", e)
+	w, b, e = PureWord("(-mama")
+	assert.Equal(t, "mama", w)
+	assert.Equal(t, "(-", b)
+	assert.Equal(t, "", e)
 }
 
 func TestPureWord_NoChange(t *testing.T) {
-	w, e := PureWord("<mama>")
-	assert.Equal(t, "<mama>", w)
+	w, b, e := PureWord("„mama“")
+	assert.Equal(t, "„mama“", w)
+	assert.Equal(t, "", b)
 	assert.Equal(t, "", e)
+}
+
+func TestTrimQuotes(t *testing.T) {
+	s1, s2, s3 := TrimWord("<mama>", IsParentheses)
+	test3(t, s1, s2, s3, "mama", "<", ">")
+	s1, s2, s3 = TrimWord("<mama>", IsPunct)
+	test3(t, s1, s2, s3, "<mama>", "", "")
+	s1, s2, s3 = TrimWord(",..<mama>,-+", IsAllSep)
+	test3(t, s1, s2, s3, "mama", ",..<", ">,-+")
+}
+
+func test3(t *testing.T, s1, s2, s3, e1, e2, e3 string) {
+	assert.Equal(t, e1, s1)
+	assert.Equal(t, e2, s2)
+	assert.Equal(t, e3, s3)
 }
